4f9ade177292faf8b0e6d7163936ac5c28a181de
Ahmad shamsedine <ahmadshamsedine@gmail.com>
2025-02-12 15:17:52 +0200

    giving the avarage of three numbers

--- calculater.py
+++ calculater.py
@@ -1,1 +1,9 @@
 print("hello enter three numbers to calculate the avaerage:")
+number1=int(input("enter the first number:"))
+number2=int(input("enter the seconde number:"))
+number3=int(input("enter the third number:"))
+
+sum= (number1 + number2 + number3)
+avarage=int(sum/3 )
+print ("the avarage of the 3 numbers is : " + str(avarage)  )
+
